3640dd51193fb27d62f2c2efc77377f0c1664b6e
JulietMumbi <juliet.ndambuki@strathmore.edu>
2025-06-18 17:40:01 +0300

    made changes in login

--- login.php
+++ login.php
@@ -19,9 +19,9 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         $_SESSION['name'] = $result['name'];
 
         // Redirect to dashboard
-        if ($row['role'] == 'Member') {
+        if ($user['role'] == 'Member') {
             header("Location: ../dashboard/member_dashboard.php");
-        } elseif ($row['role'] == 'Treasurer') {
+        } elseif ($user['role'] == 'Treasurer') {
             header("Location: ../dashboard/treasurer_dashboard.php");
         }
         exit();
