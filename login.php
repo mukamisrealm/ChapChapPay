9f47b87f2f91cdd0180f4a56c8ed41598ee0ee0d
JulietMumbi <juliet.ndambuki@strathmore.edu>
2025-06-21 18:03:13 +0300

    login page

--- login.php
+++ login.php
@@ -1,4 +1,9 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+session_start();
+
 include 'db.php';
 session_start();
 
@@ -18,6 +23,8 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         $_SESSION['role'] = $result['role'];
         $_SESSION['name'] = $result['name'];
 
+         echo "Role: " . $_SESSION['role'];  // DEBUG LINE
+    exit();
         // Redirect to dashboard
         if ($result['role'] === 'Member') {
             header("Location: memberdashboard.php");
