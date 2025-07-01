3f1e565c15aeb6858a50d5a3f2d50812f6539678
JulietMumbi <juliet.ndambuki@strathmore.edu>
2025-07-01 17:26:24 +0300

    added nav bar

--- treasurerdashboard.php
+++ treasurerdashboard.php
@@ -22,11 +22,32 @@ $name = $_SESSION['name'];
 </head>
 <body class="bg-light">
 
+<!-- Navbar -->
 <nav class="navbar navbar-expand-lg navbar-dark bg-success">
   <div class="container-fluid">
     <a class="navbar-brand" href="#">ChapChapPay</a>
-    <div class="d-flex">
-      <a href="../logout.php" class="btn btn-outline-light">Logout</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+      aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item"><a class="nav-link" href="treasurerdashboard.php">Dashboard</a></li>
+        <li class="nav-item"><a class="nav-link" href="view_members.php">View Members</a></li>
+        <li class="nav-item"><a class="nav-link" href="view_contrib.php">View Contributions</a></li>
+        <li class="nav-item"><a class="nav-link" href="view_fine.php">View Fines</a></li>
+        <li class="nav-item"><a class="nav-link" href="schedule_payout.php">Schedule Payout</a></li>
+        <li class="nav-item"><a class="nav-link" href="view_reports.php">View Reports</a></li>
+        <li class="nav-item"><a class="nav-link" href="add_fine.php">Add Fine</a></li>
+        <li class="nav-item"><a class="nav-link" href="add_contrib.php">Add Contribution</a></li>
+         <li class="nav-item"><a class="nav-link" href="view_requests.php">View Requests</a></li>
+          <li class="nav-item"><a class="nav-link" href="view_payout.php">View Payout</a></li>
+      </ul>
+      <span class="navbar-text me-3">
+        Welcome, <?php echo htmlspecialchars($name); ?>
+      </span>
+      <a href="logout.php" class="btn btn-outline-light btn-sm">Logout</a>
     </div>
   </div>
 </nav>
