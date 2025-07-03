af75c0227031f30e4d6760e9159622a1a3434b89
JulietMumbi <juliet.ndambuki@strathmore.edu>
2025-07-03 14:02:10 +0300

    implemented member navbar

---
+++ navbar_member.php
@@ -0,0 +1,19 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">ChapChapPay</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navContent">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navContent">
+      <ul class="navbar-nav me-auto">
+        <li class="nav-item"><a class="nav-link" href="memberdashboard.php">Dashboard</a></li>
+        <li class="nav-item"><a class="nav-link" href="view_contrib.php">My Contributions</a></li>
+        <li class="nav-item"><a class="nav-link" href="view_fine.php">My Fines</a></li>
+        <li class="nav-item"><a class="nav-link" href="view_payout.php">Payout Schedule</a></li>
+        <li class="nav-item"><a class="nav-link" href="view_reports.php">Reports</a></li>
+        <li class="nav-item"><a class="nav-link" href="make_request.php">Request</a></li>
+      </ul>
+      <a href="logout.php" class="btn btn-outline-light btn-sm">Logout</a>
+    </div>
+  </div>
+</nav>
