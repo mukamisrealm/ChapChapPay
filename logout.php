8de6c099042cf26494895e60f2ef7fcd42de4a4c
JulietMumbi <juliet.ndambuki@strathmore.edu>
2025-07-03 10:53:53 +0300

    restored logout functioning

--- logout.php
+++ logout.php
@@ -3,6 +3,6 @@ session_start();          // Start session (to destroy it)
 session_unset();          // Unset all session variables
 session_destroy();        // Destroy the session
 
-header("Location: login.php");  // Redirect to login
+header("Location: register_login.php");  // Redirect to login
 exit();                   // Make sure no other code runs
 ?>
